32614f48717da261d423e6245cdfa02d79344bc9
zhengtaoyu <717828758@qq.com>
2019-08-27 09:41:53 +0800

    1

--- src/common.php
+++ src/common.php
@@ -6,18 +6,10 @@
 
 namespace cyr\junhuan;
 
-define('PAY_URL', 'https://www.smtsvspay.com/cz/interface/wxpt/payOrder.do');
-define('PAY_REWRITE', 'https://www.smtsvspay.com/cz/interface/wxpt/notificationConfirm.do');
-define('PAY_QUERY', 'https://www.smtsvspay.com/cz/interface/wxpt/findOrder.do');
-define('PAY_GETCERT', 'https://www.smtsvspay.com/cz/interface/wxpt/PaymentCertification.do ');
 
 define('PAY_XF_SEARCH', 'http://www.smtsvspay.com/test/fjfl_xfmf_js/api/service');
 define('PAY_XF_PAY', 'http://www.smtsvspay.com/test/fjfl_xfmf_js/api/pay');
 
-define('TRANSAC_GETCERT', 'TMRI_PAYMENT_CERTIFICATION');
-define('TRANSAC_PAYCREATE', 'TMRI_ORDER_CREATE');
-define('TRANSAC_REWRITE', 'TMRI_ORDER_NOTIFY_CONFIRM');
-define('TRANSAC_QUERY', 'TMRI_ORDER_QUERY');
 //学法免分
 define('TRANSAC_XF_QUERY', 'TRIM_XFMF_JKBHCX');
 define('TRANSAC_XF_JF', 'TRIM_XFMF_JF');
